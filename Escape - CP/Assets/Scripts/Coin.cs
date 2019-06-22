f7e5b7e888288131b2572c4a481058f37a2680ee
jmorenomorales <32261253+jmorenomorales@users.noreply.github.com>
2019-06-22 15:39:05 +0200

    Mapa Final con Moneda
    Se han cambiado el color de los bloques y añadido una moneda que permite reiniciar el mapa cuando el jugador llega a cogerla. Es la forma de ganar.

---
+++ Escape - CP/Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Coin : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Final Simulation");
+        }
+    }
+}
